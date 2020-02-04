ebf59f5f5f725d24ab93df128a2aaa886baddef7
Devasri Warrier <devasri.warrier@mavs.uta.edu>
2020-02-03 23:05:47 -0600

    formatting C++

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -1,15 +1,10 @@
 #include "color.h"
 
-std::string Color::to_string()
-{
-  rainbow = std::to_string(red)+";"+std::to_string(green)+";"+std::to_string(blue);
+std::string Color::to_string() {
+  rainbow = std::to_string(red) + ";" + std::to_string(green) + ";" + std::to_string(blue);
   return rainbow;
 };
 
-
-void Color::colorize(std::string rainbow2, std::string text)
-{
-  std::cout << "\033[38;2;"+rainbow2+";177m"<< text<< "\033[0m" <<std::endl;
+void Color::colorize(std::string rainbow2, std::string text) {
+  std::cout << "\033[38;2;" + rainbow2 + ";177m" << text << "\033[0m" << std::endl;
 };
-
-
--- P02/full_credit/main.cpp
+++ P02/full_credit/main.cpp
@@ -1,41 +1,36 @@
 #include "color.h"
 
-
-
-int main() { 
-Color red(255,0,0);
-Color green(0,255,0);
-Color blue(0,0,255);
+int main() {
+  Color red(255, 0, 0);
+  Color green(0, 255, 0);
+  Color blue(0, 0, 255);
 
   int r, g, b;
-std::string temp1, temp2, temp3;
-std::string temp = "Red";
-std::string temp4 = "Green";
-std::string temp5 = "Blue";
+  std::string temp1, temp2, temp3;
+  std::string temp = "Red";
+  std::string temp4 = "Green";
+  std::string temp5 = "Blue";
 
-//all of temps (1,2,3,6) are holding return output from to_string function
+  //all of temps (1,2,3,6) are holding return output from to_string function
 
-temp1 = red.to_string();
-temp2 = green.to_string();
-temp3 = blue.to_string();
+  temp1 = red.to_string();
+  temp2 = green.to_string();
+  temp3 = blue.to_string();
 
- red.colorize(temp1, temp);
- green.colorize(temp2, temp4);
- blue.colorize(temp3, temp5);
+  red.colorize(temp1, temp);
+  green.colorize(temp2, temp4);
+  blue.colorize(temp3, temp5);
 
-std::cout << "Please enter 3 integer values representing red, green, blue (spaces inbetween): ";
-std::cin>>r>>g>>b;
+  std::cout << "Please enter 3 integer values representing red, green, blue (spaces inbetween): ";
+  std::cin >> r >> g >> b;
 
-std::string output = "(" + std::to_string(r) + "," + std::to_string(g) + "," + std::to_string(b) + ")";
+  std::string output = "(" + std::to_string(r) + "," + std::to_string(g) + "," + std::to_string(b) + ")";
 
+  std::string temp6;
 
+  Color color(r, g, b);
+  temp6 = color.to_string();
+  color.colorize(temp6, output);
 
-std::string temp6;
-
-Color color(r,g,b);
-temp6 = color.to_string();
-color.colorize(temp6, output);
-
-
-return 0;
+  return 0;
 }
