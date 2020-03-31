5833e98086b356f16c7496190efceee504cf81ac
Devasri Warrier <devasri.warrier@mavs.uta.edu>
2020-03-30 21:11:15 -0500

    mockup application complete

--- elsa/mainwin.cpp
+++ elsa/mainwin.cpp
@@ -1,4 +1,5 @@
 #include "mainwin.h"
+#include "entrydialog.h"
 #include <iostream> // for std::cerr logging
 
 Mainwin::Mainwin() : store{nullptr} {
@@ -8,7 +9,7 @@ Mainwin::Mainwin() : store{nullptr} {
     // /////////////////
 
     set_default_size(400, 200);
-    set_title("The Game of ");
+    set_title("ELSA!!!!!!!!!!");
 
     // Put a vertical box container as the Window contents
     Gtk::Box *vbox = Gtk::manage(new Gtk::VBox);
@@ -21,32 +22,82 @@ Mainwin::Mainwin() : store{nullptr} {
     vbox->pack_start(*menubar, Gtk::PACK_SHRINK, 0);
     // vbox->add(*menubar);
 
-    //     F I L E
+    //F I L E
     // Create a File menu and add to the menu bar
     Gtk::MenuItem *menuitem_file = Gtk::manage(new Gtk::MenuItem("_File", true));
     menubar->append(*menuitem_file);
     Gtk::Menu *filemenu = Gtk::manage(new Gtk::Menu());
     menuitem_file->set_submenu(*filemenu);
-
-    //         Q U I T
+    //Q U I T
     // Append Quit to the File menu
     Gtk::MenuItem *menuitem_quit = Gtk::manage(new Gtk::MenuItem("_Quit", true));
     menuitem_quit->signal_activate().connect([this] {this->on_quit_click();});
     filemenu->append(*menuitem_quit);
 
-    //     H E L P
+    //H E L P
     // Create a Help menu and add to the menu bar
     Gtk::MenuItem *menuitem_help = Gtk::manage(new Gtk::MenuItem("_Help", true));
     menubar->append(*menuitem_help);
     Gtk::Menu *helpmenu = Gtk::manage(new Gtk::Menu());
     menuitem_help->set_submenu(*helpmenu);
-
-    //           A B O U T
+    //A B O U T
     // Append About to the Help menu
     Gtk::MenuItem *menuitem_about = Gtk::manage(new Gtk::MenuItem("_About", true));
     menuitem_about->signal_activate().connect([this] {this->on_about_click();});
     helpmenu->append(*menuitem_about);
 
+//V I E W
+    // Create a view menu and add to the menu bar
+    Gtk::MenuItem *menuitem_view = Gtk::manage(new Gtk::MenuItem("_View", true));
+    menubar->append(*menuitem_view);
+    Gtk::Menu *viewmenu = Gtk::manage(new Gtk::Menu());
+    menuitem_view->set_submenu(*viewmenu);
+
+//C U S T O M E R
+    // Append customer to the File menu
+    Gtk::MenuItem *menuitem_customer = Gtk::manage(new Gtk::MenuItem("_Customer", true));
+    menuitem_customer->signal_activate().connect([this] {this->on_view_customer_click();});
+    viewmenu->append(*menuitem_customer);
+
+//P E R I P H E R A L
+    // Append peripheral to the File menu
+    Gtk::MenuItem *menuitem_peripheral = Gtk::manage(new Gtk::MenuItem("_Peripheral", true));
+    menuitem_peripheral->signal_activate().connect([this] {this->on_view_peripheral_click();});
+    viewmenu->append(*menuitem_peripheral);
+
+//D E S K T O P
+    Gtk::MenuItem *menuitem_desktop = Gtk::manage(new Gtk::MenuItem("Desktop", true));
+    menuitem_desktop->signal_activate().connect([this] {this->on_view_desktop_click();});
+    viewmenu->append(*menuitem_desktop);
+//O R D E R
+    Gtk::MenuItem *menuitem_order = Gtk::manage(new Gtk::MenuItem("_Order", true));
+    menuitem_order->signal_activate().connect([this] {this->on_view_order_click();});
+    viewmenu->append(*menuitem_order);
+
+//I N S E R T
+    // Create a insert menu and add to the menu bar
+    Gtk::MenuItem *menuitem_insert = Gtk::manage(new Gtk::MenuItem("_Insert", true));
+    menubar->append(*menuitem_insert);
+    Gtk::Menu *insertmenu = Gtk::manage(new Gtk::Menu());
+    menuitem_insert->set_submenu(*insertmenu);
+//C U S T O M E R
+    // Append customer to the File menu
+    Gtk::MenuItem *menuitem__insert_customer = Gtk::manage(new Gtk::MenuItem("_Customer", true));
+    menuitem__insert_customer->signal_activate().connect([this] {this->on_insert_customer_click();});
+    insertmenu->append(*menuitem__insert_customer);
+//P E R I P H E R A L
+    // Append peripheral to the File menu
+    Gtk::MenuItem *menuitem_insert_peripheral = Gtk::manage(new Gtk::MenuItem("_Peripheral", true));
+    menuitem_insert_peripheral->signal_activate().connect([this] {this->on_insert_peripheral_click();});
+    insertmenu->append(*menuitem_insert_peripheral);
+//D E S K T O P
+    Gtk::MenuItem *menuitem_insert_desktop = Gtk::manage(new Gtk::MenuItem("Desktop", true));
+    menuitem_insert_desktop->signal_activate().connect([this] {this->on_insert_desktop_click();});
+    insertmenu->append(*menuitem_insert_desktop);
+//O R D E R
+    Gtk::MenuItem *menuitem_insert_order = Gtk::manage(new Gtk::MenuItem("_Order", true));
+    menuitem_insert_order->signal_activate().connect([this] {this->on_insert_order_click();});
+    insertmenu->append(*menuitem_insert_order);
 
     //     Q U I T
     // Push the quit botton all the way to the right by setting set_expand true
@@ -80,9 +131,7 @@ Mainwin::~Mainwin() { }
 
 // COMBINED observer / callback
 
-void Mainwin::on_quit_click() {
-    close();
-}
+void Mainwin::on_quit_click() { close(); }
 
 void Mainwin::on_about_click() {
     Gtk::AboutDialog dialog;
@@ -101,6 +150,18 @@ void Mainwin::on_about_click() {
     dialog.set_artists(artists);
     dialog.run();
 }
+
+void Mainwin::on_view_customer_click() {};
+void Mainwin::on_view_peripheral_click() {};  
+void Mainwin::on_view_desktop_click(){};
+void Mainwin::on_view_order_click(){};
+
+void Mainwin::on_insert_customer_click(){
+};
+
+void Mainwin::on_insert_peripheral_click(){};
+void Mainwin::on_insert_desktop_click(){};
+void Mainwin::on_insert_order_click(){}
 
 // /////////////////
 // U T I L I T I E S
