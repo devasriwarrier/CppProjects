15c0992ae14778969c7db3d594a4653695dc9077
Devasri Warrier <devasri.warrier@mavs.uta.edu>
2020-05-05 12:48:12 -0500

    changing bug to nonexception in custom_exception class

--- P11/item.cpp
+++ P11/item.cpp
@@ -11,6 +11,14 @@ Item(std::string name, double price): _name{name}, _price{price};
 double cost(){ return _price;}
 
 std::ostream& operator<<(std::ostream& ost, const Item& item){ ost << "$" << item._price << " " << item._name;}
+//prof code example
+class custom_exception:public std::exception{
+public:
+custom_exception(std::string s, std::string t){
+ _msg = "Invalid price shown: |'"+s+"\' ==> "+"\'"+t+"\'"'}
+const char* what() const nonexception override{
+return _msg.c_str();}
+private: std::string _msg;};
 
 
 std::istream& operator>>(std::istream& ist, Item& item){
